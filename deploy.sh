03193c696002b6acc19ac3aa5901aaa48402b44d
vdegenne <vdegenne@gmail.com>
2021-12-25 13:16:42 +0100

    data update Sat Dec 25 13:16:42 CET 2021

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm ./data/*
-./src/vortex.ts binance -u h -w 49
+#./src/vortex.ts binance -u h -w 49
+./src/vortex.ts binance -u d -w 60
 git add .
 git commit -m "data update $(date)"
 git push
